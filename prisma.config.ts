711cefca9c365b50a45f50c5c04df26f7a8f2aef
Ayomiidey <quadriayomiidey@gmail.com>
2026-08-05 17:25:38 +0100

    initial migration

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,13 @@
-import "dotenv/config";
-import { defineConfig } from "prisma/config";
+import 'dotenv/config'
+import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrate: {
-    async adapter() {
-      const url = process.env.DATABASE_URL!;
-      const { PrismaPg } = await import("@prisma/adapter-pg");
-      return new PrismaPg({ connectionString: url });
-    },
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
+    seed: 'tsx prisma/seed.ts',
+  },
+  datasource: {
+    url: env('DATABASE_URL'),
   },
 });
